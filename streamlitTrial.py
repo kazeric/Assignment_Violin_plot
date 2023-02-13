1ccaea025b83b841f36da1df9a9f51a872a6f3ff
kazeric <kahindie873@gmail.com>
2023-02-13 23:05:31 +0300

    second  commit

--- streamlitTrial.py
+++ streamlitTrial.py
@@ -8,12 +8,12 @@ data=pd.read_csv('gapminder_with_codes.csv')
 data_2007=data[data['year']==2007]
 
 fig = plt.figure(figsize=(9,7))
-plt.title("LIFE E")
+plt.title("LIFE EXPECTANCY")
 sns.violinplot(data=data_2007,x='lifeExp',scale='count')
 st.pyplot(fig)
 
 fig = plt.figure(figsize=(9,7))
-plt.title("hello again")
+plt.title("POPULATION")
 sns.violinplot(data=data_2007,x='pop',scale='count')
 st.pyplot(fig)
 
@@ -30,4 +30,9 @@ st.pyplot(fig)
 fig = plt.figure(figsize=(9,7))
 plt.title("hello again")
 sns.violinplot(data=data_2007,x='pop',y='continent',scale='count')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(9,7))
+plt.title("hello again")
+sns.violinplot(data=data_2007,x='gdpPercap',y='continent',scale='count')
 st.pyplot(fig)
