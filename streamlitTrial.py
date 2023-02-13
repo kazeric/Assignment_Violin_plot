7f3525a744e957ac6a3988e2cf4d3429c4a893e3
kazeric <kahindie873@gmail.com>
2023-02-13 18:46:33 +0300

    my_code

---
+++ streamlitTrial.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data=pd.read_csv('gapminder_with_codes.csv')
+
+data_2007=data[data['year']==2007]
+
+fig = plt.figure(figsize=(9,7))
+plt.title("LIFE E")
+sns.violinplot(data=data_2007,x='lifeExp',scale='count')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(9,7))
+plt.title("hello again")
+sns.violinplot(data=data_2007,x='pop',scale='count')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(9,7))
+plt.title("hello again")
+sns.violinplot(data=data_2007,x='gdpPercap',scale='count')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(9,7))
+plt.title("hello again")
+sns.violinplot(data=data_2007,x='lifeExp', y='continent',scale='count')
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(9,7))
+plt.title("hello again")
+sns.violinplot(data=data_2007,x='pop',y='continent',scale='count')
+st.pyplot(fig)
